b75e86205e949800c5c065183a4d774cbc95ccb4
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2018-05-18 12:52:13 +0700

    update tugas 7 dan 8

---
+++ exercise-8.js
@@ -0,0 +1,11 @@
+function cariPelaku(str) {
+  return (str.match(/abc/gi).length);
+  // you can only write your code here!
+}
+
+// TEST CASES
+console.log(cariPelaku('mabcvabc')); // 2
+console.log(cariPelaku('abcdabdc')); // 1
+console.log(cariPelaku('bcabcac')); // 1
+console.log(cariPelaku('abcabcabc')); // 3
+console.log(cariPelaku('babcbacabc')); // 2
